b77385741cbadb70921663779bd0a2fc47198af8
pvedula1 <90353816+pvedula1@users.noreply.github.com>
2021-10-13 16:35:39 -0400

    update 1.0

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,22 @@
 module.exports = {
   chainWebpack: config => {
+    config.resolve.symlinks(false)
+
     config.plugin("html").tap(args => {
       args[0].title = "LRN Lab: Pain App Dashboard";
       return args;
     });
   }
 };
+
+module.exports = {
+  chainWebpack: config => {
+    config.resolve.symlinks(false)
+
+    config.plugin("html").tap(args => {
+      args[0].title = "LRN Lab: Pain App Dashboard";
+      return args;
+    });
+  }
+};
+
